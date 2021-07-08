61235e4bf536e526c12917e53a0fc0a2ff3929da
gabi <gabrielacunarro@gmail.com>
2021-07-08 19:21:06 -0300

    commit

--- 1-EntradaSalida/jsEntradaSalida-10.js
+++ 1-EntradaSalida/jsEntradaSalida-10.js
@@ -6,18 +6,18 @@ en el cuadro de texto "RESULTADO"*/
 function mostrarAumento()
 {
 	let importe;
-	let nuevoimporte;
+	let nuevo_importe;
 	let descuento;
 
 	importe = parseFloat(document.getElementById("txtIdImporte").value);
 
-	descuento = (importe * 25) / 100;
+	descuento = importe *.25;
 
-	nuevoimporte = importe - descuento;
+	nuevo_importe = importe - descuento;
 
 	// la respuesta tiene que salir en el cuadro de texto debo llevar el resultado alli 
 	
-	document.getElementById ("txtIdResultado").value = nuevoimporte;
+	document.getElementById ("txtIdResultado").value = nuevo_importe;
 
 
 
