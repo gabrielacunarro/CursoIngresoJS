101f9cee3e831e6463a28e4c1e874c7adeb8971d
gabi <gabrielacunarro@gmail.com>
2021-07-08 19:18:36 -0300

    nuevo commit

--- 1-EntradaSalida/jsEntradaSalida-09.js
+++ 1-EntradaSalida/jsEntradaSalida-09.js
@@ -9,13 +9,12 @@ function mostrarAumento()
   let nuevo_sueldo;
   let aumento;
   // para calcular nuevo sueldo debo sumarle al original al aumento
-  //el calculo de porcentaje de un valor lo consigo multiplicando el valor por el porcentaje y lo divido por 100
 
   sueldo = parseFloat(document.getElementById("txtIdSueldo").value);
 
   // recordar que en estos casos se usa parsefloat ya que el sueldo no es necesariamente un Z (num entero)
   
-  aumento = sueldo * 10 / 100;
+  aumento = sueldo * .1;
 
   nuevo_sueldo = sueldo + aumento;
 
