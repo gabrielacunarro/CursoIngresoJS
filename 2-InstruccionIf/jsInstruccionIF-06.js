9c09bb9fa3d347348ebcd38515ff031a4fd06344
gabi <gabrielacunarro@gmail.com>
2021-07-13 20:14:59 -0300

    nuevo commit

--- 2-InstruccionIf/jsInstruccionIF-06.js
+++ 2-InstruccionIf/jsInstruccionIF-06.js
@@ -4,13 +4,20 @@ function mostrar()
 
 	edad = parseInt(document.getElementById("txtIdEdad").value);
 
-	if 
-		
+	if (edad >= 18)
+	{
+		alert ("Es adulto")
+	} else {
+	  if (edad <=12) {
+			alert("Es niño");
+	  } else {
+			alert ("Es adolescente");
+		}
 
 		
+	}
+
 	
-  
-
-
+	
 
 }//FIN DE LA FUNCIÓN
