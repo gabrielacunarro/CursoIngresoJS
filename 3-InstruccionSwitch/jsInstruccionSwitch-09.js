35afb7f9748aa27d23437b436ca966d51bdf5393
gabi <gabrielacunarro@gmail.com>
2021-07-14 22:06:16 -0300

    nuevo commit

--- 3-InstruccionSwitch/jsInstruccionSwitch-09.js
+++ 3-InstruccionSwitch/jsInstruccionSwitch-09.js
@@ -1,6 +1,45 @@
 function mostrar()
 {
-	var estacionIngresada =txtIdEstacion.value;
-	alert(estacionIngresada);
+	let estacion;
+	let destino;
+	let precio;
+
+	estacion = document.getElementById("txtIdEstacion").value;
+	destino = document.getElementById("txtIdDestino").value;
+
+	switch (estacion){
+		case "Invierno":
+			case "Bariloche":
+			case "Cataratas":
+			case "Mar del plata":
+			case "Cordoba":
+				break;
+		case "Primavera":
+			case "Bariloche":
+			case "Cataratas":
+			case "Mar del plata":
+			case "Cordoba":
+				break;
+		case "Otoño":
+			case "Bariloche":
+			case "Cataratas":
+			case "Mar del plata":
+			case "Cordoba":
+				break;
+		case "Verano":	
+		case "Bariloche":			
+		case "Cataratas":		
+		case "Mar del plata":
+		case "Cordoba":
+			break;
+	}		
+			
+
+
+
+
+
+
+
 
 }//FIN DE LA FUNCIÓN
